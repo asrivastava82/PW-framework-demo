31d042e81e7893b2cb63a562fc33007647db92c7
asrivastava82 <78613350+asrivastava82@users.noreply.github.com>
2026-01-29 20:32:05 +0530

    changes in playwright config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html"], ["list"], ["allure-playwright"]],
+  reporter: [["html", { never: true }], ["list"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. Set from env so tests can use relative paths. */
@@ -33,6 +33,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+    headless: true,
+    launchOptions: {
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    },
   },
 
   /* Configure projects for major browsers */
