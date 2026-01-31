5ec434c2fc3f68ca6dd81793b7033cdbd5677018
asrivastava82 <78613350+asrivastava82@users.noreply.github.com>
2026-01-31 14:18:34 +0530

    modified config

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,11 +25,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ["html", { outputFolder: "playwright-report", never: true }],
-    ["list"],
-    ["allure-playwright"],
-  ],
+  reporter: process.env.CI
+    ? [["html", { open: "never" }]]
+    : [["html", { open: "on-failure" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. Set from env so tests can use relative paths. */
