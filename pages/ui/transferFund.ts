1405c764cf9b0d5d15ce08b5d97360bbed52b178
asrivastava82 <78613350+asrivastava82@users.noreply.github.com>
2026-02-11 21:28:57 +0530

    rename all the test files

---
+++ pages/ui/transferFund.ts
@@ -0,0 +1,5 @@
+// import { test, Page, Locator, expect } from "@playwright/test";
+// import { BasePage } from "./BasePage";
+
+// export class TransferFundPage extends BasePage {
+//   fromAccount: Locator
